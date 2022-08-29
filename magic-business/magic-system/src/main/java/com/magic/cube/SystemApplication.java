817da6c527b287ad0cf957ba6ac3f61fbf4854d9
TJStep2018 <550871740@qq.com>
2022-08-30 00:44:18 +0800

    init

---
+++ magic-business/magic-system/src/main/java/com/magic/cube/SystemApplication.java
@@ -0,0 +1,18 @@
+package com.magic.cube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * 
+ * @author: TJ
+ * @date:  2022-08-30
+ **/
+@SpringBootApplication
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
